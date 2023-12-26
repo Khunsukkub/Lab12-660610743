0cbd8bf4583bcbbbc0615939812d0b409a211af6
Khun <khunsuek@eiei.com>
2023-12-26 14:23:40 +0700

    Updated

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,9 +1,24 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(int num);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+int fibonacci(int num){
+
+    if(num == 0 || num == 1){
+        return num;
+    } else if(num > 1){
+        return fibonacci(num-1)+fibonacci(num-2);
+    } else{
+        cout << "incorrect input";
+        return 0;
+    }
+
 }
